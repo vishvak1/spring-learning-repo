1a01541e09318e64186eebf46f6946685c57f662
vishvak1 <vishvak.vishvak98@gmail.com>
2023-10-26 20:00:51 +0530

    Removed unwanted imports

--- src/main/java/com/vishvak/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/vishvak/demo/repositories/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package com.vishvak.demo.repositories;
 import com.vishvak.demo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
