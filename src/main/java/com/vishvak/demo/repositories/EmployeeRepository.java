a5fe1e8bb8f946e30fbe75d51180c814014c288e
vishvak1 <vishvak.vishvak98@gmail.com>
2023-10-26 23:09:55 +0530

    Custom Query example added

--- src/main/java/com/vishvak/demo/repositories/EmployeeRepository.java
+++ src/main/java/com/vishvak/demo/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.vishvak.demo.repositories;
 import com.vishvak.demo.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -14,5 +15,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // If you want to find all the employees with the same last name
     // All you have to do is just suffix the word find with ByColumnNameInCamelCase. like this
     public List<Employee> findByLastName(String lastName);
+
+    @Query("SELECT e FROM Employee e WHERE e.email = :mailId")
+    public List<Employee> findByMailId(String mailId);
 
 }
